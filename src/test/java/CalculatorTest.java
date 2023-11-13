24e3f4b63598f89e7fcc84014e9158d0cc972811
Ann-Fox <fx8565@mail.ru>
2023-11-13 19:00:28 +0500

    homework1 task 1

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -7,6 +7,101 @@ import static org.assertj.core.api.Fail.fail;
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalculatorTest {
+    private Calculator calculator;
+
+    @BeforeAll
+    static void beforeAll() {
+// Здесь могут быть глобальные операции инициализации
+    }
+
+    @AfterAll
+    static void afterAll() {
+// Здесь могут быть глобальные операции завершения
+    }
+
+    @BeforeEach
+    void setUp() {
+        calculator = new Calculator();
+    }
+
+    @AfterEach
+    void tearDown() {
+        calculator = null;
+    }
+
+    /*
+       Задание 1.1 ** В классе Calculator создайте метод calculateDiscount, который принимает сумму покупки и процент
+       скидки и возвращает сумму с учетом скидки. Ваша задача - проверить этот метод с использованием библиотеки
+       AssertJ. Если метод calculateDiscount получает недопустимые аргументы, он должен выбрасывать исключение
+       ArithmeticException.
+   */
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0, 101})
+    @DisplayName("Проверка процентов")
+    void calculatingDiscount(int percent) {
+        int purchaseAmount = 1000;
+        assertThrows(ArithmeticException.class, () -> calculator.calculatingDiscount(purchaseAmount, percent));
+    }
+
+    @Test
+    @DisplayName("Проверка суммы денег")
+    void calculatingDiscount() {
+        int purchaseAmount = -1;
+        int purchasePercent = 50;
+        assertThrows(ArithmeticException.class, () -> calculator.calculatingDiscount(purchaseAmount, purchasePercent));
+        assertThrows(ArithmeticException.class, () -> calculator.calculatingDiscount(Integer.MAX_VALUE + 1,
+                purchasePercent));
+    }
+
+    /*
+      Задание 1.1 **
+  */
+    @Test
+    @DisplayName("Проверка суммирования")
+    void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    @DisplayName("Проверка вычитания")
+    void testSubtract() {
+        assertEquals(1, calculator.subtract(3, 2));
+    }
+
+    @Test
+    @DisplayName("Проверка умножения")
+    void testMultiply() {
+        assertEquals(6, calculator.multiply(2, 3));
+    }
+
+    @Test
+    @DisplayName("Проверка деления")
+    void testDivide() {
+        Assertions.assertEquals(2.0, calculator.divide(4, 2));
+    }
+
+    @Test
+    @DisplayName("Проверка деления на ноль")
+    void testDivideByZero() {
+        assertThrows(IllegalArgumentException.class, () -> calculator.divide(1, 0));
+    }
+
+    @Test
+    @Disabled("Этот тест отключен")
+    void disabledTest() {
+        fail("Не будет выполнен");
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3})
+    @DisplayName("Параметризованный тест для умножения на 2")
+    void testMultiplyWithParameters(int arg) {
+        assertEquals(arg * 2, calculator.multiply(arg, 2));
+    }
+}
+
 //public class CalculatorTest {
 //    private Calculator calculator;
 //
@@ -56,69 +151,3 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 //        assertEquals(arg + 2, calculator.add(arg, 2));
 //    }
 //}
-class CalculatorTest {
-    private Calculator calculator;
-
-    @BeforeAll
-    static void beforeAll() {
-// Здесь могут быть глобальные операции инициализации
-    }
-
-    @AfterAll
-    static void afterAll() {
-// Здесь могут быть глобальные операции завершения
-    }
-
-    @BeforeEach
-    void setUp() {
-        calculator = new Calculator();
-    }
-
-    @AfterEach
-    void tearDown() {
-        calculator = null;
-    }
-
-    @Test
-    @DisplayName("Проверка суммирования")
-    void testAdd() {
-        assertEquals(5, calculator.add(2, 3));
-    }
-
-    @Test
-    @DisplayName("Проверка вычитания")
-    void testSubtract() {
-        assertEquals(1, calculator.subtract(3, 2));
-    }
-
-    @Test
-    @DisplayName("Проверка умножения")
-    void testMultiply() {
-        assertEquals(6, calculator.multiply(2, 3));
-    }
-
-    @Test
-    @DisplayName("Проверка деления")
-    void testDivide() {
-        Assertions.assertEquals(2.0, calculator.divide(4, 2));
-    }
-
-    @Test
-    @DisplayName("Проверка деления на ноль")
-    void testDivideByZero() {
-        assertThrows(IllegalArgumentException.class, () -> calculator.divide(1, 0));
-    }
-
-    @Test
-    @Disabled("Этот тест отключен")
-    void disabledTest() {
-        fail("Не будет выполнен");
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = {0, 1, 2, 3})
-    @DisplayName("Параметризованный тест для умножения на 2")
-    void testMultiplyWithParameters(int arg) {
-        assertEquals(arg * 2, calculator.multiply(arg, 2));
-    }
-}
